003a987c67bb0e77d7ba9ba23fbd70a793f75a89
unknown <isfand.zabirov@gmail.com>
2021-07-14 20:02:15 +0500

    Homework added

--- main.go
+++ main.go
@@ -3,8 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println("I am Isfandiyor Zabirov")
+	//fmt.Println("Hello World")
+	//fmt.Println("I am Isfandiyor Zabirov")
+
+	minAmount := 5_000
+	purchase := 15_000
+	percentCashback := 15
+	limitCashback := 2_000
+	cashback := 0
+	const fullPercent int = 100
+
+	// 1. Можно ли дать кэшбек
+	if purchase >= minAmount {
+		cashback = purchase * percentCashback / fullPercent
+		fmt.Println("Prediction cashback is", cashback)
+
+	}
+	// 2. Превышание кэшбека
+	if cashback > limitCashback {
+		cashback = limitCashback
+	}
+	fmt.Println("Cashback is", cashback)
 }
 
 //git init
